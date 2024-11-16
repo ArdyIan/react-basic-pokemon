55cb93b79e4568b0bc53fe9a19a1e0c1019b859a
Ardy1an <adithardiyanto0@gmail.com>
2024-11-16 23:36:12 +0700

    condition

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react";
-import pokemonJSON from "../../data/pokemon.json";
+
+import pokemonsJson from "../../data/pokemon.json";
+import PokemonItem from "../PokemonItem/PokemonItem";
 
 import "./PokemonList.css";
 
-function PokemonList() {
-  const [pokemons] = useState(pokemonJSON);
+function Pokemons() {
+  const [pokemons] = useState(pokemonsJson);
+  const [filterPokemons, setFilterPokemons] = useState(pokemonsJson);
+
+  const handleSearch = (e) => {
+    let search = pokemons.filter((item) => {
+      return item.name.toLowerCase().includes(e.target.value);
+    });
+
+    setFilterPokemons(search);
+  };
 
   return (
     <div>
-      <div className="list-pokemon">
-        {pokemons.map((item) => (
-          <h1>{item.name}</h1>
-        ))}
-      </div>
+      <input type="text" placeholder="cari pokemon..." className="search" onChange={handleSearch} />
+
+      <div className="list-pokemon">{filterPokemons.length == 0 ? <div>data tidak ditemukan</div> : filterPokemons.map((item) => <PokemonItem key={item.id} pokemon={item} />)}</div>
     </div>
   );
 }
 
-export default PokemonList;
+export default Pokemons;
