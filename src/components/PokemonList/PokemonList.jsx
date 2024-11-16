3184583f286109f2704893faaa7f271182c5d27e
Ardy1an <adithardiyanto0@gmail.com>
2024-11-16 21:26:31 +0700

    store data

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -4,18 +4,17 @@ import pokemonJSON from "../../data/pokemon.json";
 import "./PokemonList.css";
 
 function PokemonList() {
-    const [pokemons] = useState(pokemonJSON);
+  const [pokemons] = useState(pokemonJSON);
 
-    return (
-        <div>
-            <div className="list-pokemon">
-            {pokemon.map((item) => (
-                    <h1>{ClipboardItem.name}</h1>
-                ))}
-            </div>
-        </div>
-    );
-
+  return (
+    <div>
+      <div className="list-pokemon">
+        {pokemons.map((item) => (
+          <h1>{item.name}</h1>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default PokemonList;
